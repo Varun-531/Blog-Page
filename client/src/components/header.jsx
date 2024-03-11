44576c63b383773273eeadec069294d90fca5a87
Varun-531 <21pa1a0531@vishnu.edu.in>
2024-03-11 11:55:33 +0530

    Add react-icons package and update errorpage component

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/icon.png";
+// import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 function header() {
   return (
     <nav>
       <div className="container nav_container">
-        <Link to='/' className='nav_logo'>
-          <img src="" alt="" />
+        <Link to="/" className="nav_logo">
+          <img src={Logo} alt="Navbar Logo" />
         </Link>
+        <ul className="nav_menu">
+          <li>
+            <Link to="/profile">Varun</Link>
+          </li>
+          <li>
+            <Link to="/create">Create Post</Link>
+          </li>
+          <li>
+            <Link to="/authors">Authors</Link>
+          </li>
+          <li>
+            <Link to="/Logout">logout</Link>
+          </li>
+        </ul>
+        <button className="nav_toggle-btn">
+        <AiOutlineClose />
+        </button>
       </div>
     </nav>
-  )
+  );
 }
 
-export default header
+export default header;
