e91c104c90f2b33fcd04c9d2b6f25ad38de24722
Varun-531 <21pa1a0531@vishnu.edu.in>
2024-03-12 22:48:39 +0530

    UserProfile update

--- client/src/pages/userprofile.jsx
+++ client/src/pages/userprofile.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Avatar5 from "../images/Avatar5.jpg";
+import { FaEdit } from "react-icons/fa";
+import { useState } from "react";
+import { FaCheck } from "react-icons/fa";
 
-function userprofile() {
+function Userprofile() {
+  const [avatar, setAvatar] = useState("");
   return (
     <div>
-      
+      <section className="profile">
+        <div className="container profile_container">
+          <Link to={"/myposts/sdfsdf"} className="btn">
+            My posts
+          </Link>
+          <div className="profile_details">
+            <div className="avatar_wrapper">
+              <div className="profile_avatar">
+                <img src={Avatar5} alt="" />
+                <form className="avatar_form">
+                  <input
+                    type="file"
+                    name="avatar"
+                    id="avatar"
+                    onChange={(e) => setAvatar(e.target.files[0])}
+                    accept="png, jpg, jpeg"
+                  />
+                  <label htmlFor="avatar">
+                    <FaEdit />
+                  </label>
+                </form>
+                <button className="profile_avatar-btn">
+                  <FaCheck />
+                </button>
+              </div>
+            </div>
+            <h1>Varun</h1>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default userprofile
+export default Userprofile;
